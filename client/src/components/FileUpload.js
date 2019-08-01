d66f6b09cbca121f5e85917fcf28ef82f64bc71a
Thomas Hessburg <44884308+TomHessburg@users.noreply.github.com>
2019-07-31 23:27:09 -0400

    uploading working! been a while since ive done this! hahaha

--- client/src/components/FileUpload.js
+++ client/src/components/FileUpload.js
@@ -1,13 +1,56 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 export default function FileUpload() {
+  const [file, setFile] = useState("");
+  const [fileName, setFileName] = useState("Choose File");
+  const [uploadedFile, setUploadedFile] = useState({});
+
+  const handelChange = e => {
+    setFile(e.target.files[0]);
+    setFileName(e.target.files[0].name);
+  };
+
+  const handelSubmit = async e => {
+    try {
+      e.preventDefault();
+
+      // to append the File() object currently stored in file
+      // we append to "file" because thats what the backend is looking for
+      const formData = new FormData();
+      formData.append("file", file);
+
+      // becaue were using a proxy, we dont have to specify base route
+      const res = await axios.post("/upload", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data"
+        }
+      });
+
+      const { fileName, filePath } = res.data;
+
+      setUploadedFile({ fileName, filePath });
+    } catch (err) {
+      if (err.response.status === 500) {
+        console.log("Server error");
+      } else {
+        console.log(err.response.data.msg);
+      }
+    }
+  };
+
   return (
     <>
-      <form>
+      <form onSubmit={handelSubmit}>
         <div className="custom-file mb-4">
-          <input type="file" className="custom-file-input" id="customFile" />
+          <input
+            type="file"
+            className="custom-file-input"
+            id="customFile"
+            onChange={handelChange}
+          />
           <label className="custom-file-label" htmlFor="customFile">
-            Choose file
+            {fileName}
           </label>
         </div>
 
